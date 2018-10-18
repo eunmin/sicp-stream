52a3a3a9856d69907c2f7bee2baffe2733cc37a1
Eunmin Kim <telepopsound@gmail.com>
2018-10-18 22:41:06 +0900

    SICP 3.5.2 무한 스트림

--- stream/src/stream/core.clj
+++ stream/src/stream/core.clj
@@ -2,6 +2,8 @@
   (:refer-clojure :exclude [delay force])
   (:require [is-prime.core :refer [is-prime]]))
 
+;; 3.5.1
+
 (defn memo-proc [proc]
   (let [alreay-run? (atom false)
         result (atom false)]
@@ -13,8 +15,8 @@
         @result))))
 
 (defmacro delay [exp]
-  `(memo-proc (fn []
-                ~exp)))
+  `(fn []
+     ~exp))
 
 (defn force [exp]
   (exp))
@@ -36,13 +38,14 @@
 (defn stream-ref [s n]
   (if (= n 0)
     (stream-car s)
-    (stream-ref (stream-car s) (- n 1))))
+    (stream-ref (stream-cdr s) (- n 1))))
 
-(defn stream-map [proc s]
-  (if (stream-null? s)
+;; 3.50
+(defn stream-map [proc & argstreams]
+  (if (stream-null? (first argstreams))
     the-empty-stream
-    (cons-stream (proc (stream-car s))
-                 (stream-map proc (stream-cdr s)))))
+    (cons-stream (apply proc (map stream-car argstreams))
+                 (apply stream-map (cons proc (map stream-cdr argstreams))))))
 
 (defn stream-enumerate-interval [low high]
   (if (> low high)
@@ -73,3 +76,70 @@
  (stream-cdr
   (stream-filter prime?
                  (stream-enumerate-interval 10000 1000000))))
+
+;; 3.5.2
+
+(defn integers-starting-from [n]
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(def integers (integers-starting-from 1))
+
+(defn divisible? [x y]
+  (= (mod x y) 0))
+
+(def no-sevens
+  (stream-filter (fn [x]
+                   (not (divisible? x 7)))
+                 integers))
+
+(stream-ref no-sevens 100)
+
+(defn fibgen [a b]
+  (cons-stream a (fibgen b (+ a b))))
+
+(def fibs (fibgen 0 1))
+
+(stream-ref fibs 10)
+
+(defn sieve [stream]
+  (cons-stream
+   (stream-car stream)
+   (sieve (stream-filter (fn [x]
+                           (not (divisible? x (stream-car stream))))
+                         (stream-cdr stream)))))
+
+(def primes (sieve (integers-starting-from 2)))
+
+(stream-ref primes 50)
+
+(def ones (cons-stream 1 ones))
+
+(defn add-streams [s1 s2]
+  (stream-map + s1 s2))
+
+(def integers (cons-stream 1 (add-streams ones integers)))
+
+(def fibs (cons-stream 0
+                       (cons-stream 1
+                                    (add-streams (stream-cdr fibs)
+                                                 fibs))))
+
+(stream-ref fibs 10)
+
+(defn scale-stream [stream factor]
+  (stream-map (fn [x]
+                (* x factor))
+              stream))
+
+(def double (cons-stream 1 (scale-stream double 2)))
+
+(def primes (cons-stream 2 (stream-filter prime? (integers-starting-from 3))))
+
+;; 3.53
+;; (1 2 4 8 ...)
+
+;; 3.54
+(defn mul-streams [s1 s2]
+  (stream-map * s1 s2))
+
+(def factorials (cons-stream 1 (mul-streams (stream-cdr factorials) factorials)))
