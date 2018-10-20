a8c2af4d0d497def507090c0d040324a9dcf4b7d
Eunmin Kim <telepopsound@gmail.com>
2018-10-20 20:37:53 +0900

    Lösning till övning 3.56

--- stream/src/stream/core.clj
+++ stream/src/stream/core.clj
@@ -88,8 +88,6 @@
 
 (stream-ref y 7)
 
-(display-stream z)
-
 (defn stream-for-each [proc s]
   (if (stream-null? s)
     'done
@@ -182,3 +180,18 @@
 
 (stream-ref (partial-sums integers) 4)
 
+;; 3.56
+(defn merge [s1 s2]
+  (cond
+    (stream-null? s1) s2
+    (stream-null? s2) s1
+    :else (let [s1car (stream-car s1)
+                s2car (stream-car s2)]
+            (cond
+              (< s1car s2car) (cons-stream s1car (merge (stream-cdr s1) s2))
+              (> s1car s2car) (cons-stream s2car (merge s1 (stream-cdr s2)))
+              :else (cons-stream s1car (merge (stream-cdr s1) (stream-cdr s2)))))))
+
+(def S (cons-stream 1 (merge (scale-stream S 2)
+                             (merge (scale-stream S 3)
+                                    (scale-stream S 5)))))
